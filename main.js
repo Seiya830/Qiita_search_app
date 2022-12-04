4c895ce5a299ac1f842242a01425b7a368663cac
Seiya830 <seiya@MacBook-Pro.local>
2022-12-04 23:46:57 +0900

    api取得の処理実装

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
 const app = Vue.createApp({
   data: () => ({
-    
+    items: null,
+    keyword: '',
+    message: ''
   }),
+  watch: {
+
+  },
+  mounted: function() {
+    this.keyword = 'JavaScript'
+    this.getAnswer()
+  },
+  methods: {
+    getAnswer: function() {
+      if(this.keyword === '') {
+        console.log('karamoji')
+        this.items = null
+        return
+      }
+
+      this.message = 'Lading...'
+      const vm = this
+      const params = { page: 1, per_page: 20, query: this.keyword }
+      axios.get('https://qiita.com/api/v2/items', { params})
+      .then(function(response){
+        //console.log(response)
+        vm.items = response.data
+      })
+      .catch(function(error){
+        vm.message = 'Error!' + error
+      })
+      .finally(function() {
+        vm.message = ''
+      })
+    }
+  }
 })
 app.mount('#app')
